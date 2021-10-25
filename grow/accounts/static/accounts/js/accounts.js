e813b1ef36815798c0949f8651254757ddcb0e30
quijibo11CZE <quijibo11cze@minehub.cz>
2021-10-25 18:01:32 +0200

    Add back button (without design)

--- grow/accounts/static/accounts/js/accounts.js
+++ grow/accounts/static/accounts/js/accounts.js
@@ -1,7 +1,20 @@
 function setActive(el) {
     document.getElementById(el).classList.add("active");
+    document.getElementById(el).classList.remove("nonactive");
     if (el == "left-block")
     {
         document.getElementsByClassName("intro-content")[0].classList.add("active");
+        document.getElementsByClassName("intro-content")[0].classList.remove("nonactive");
+    }
+}
+
+function setNonActive(el, event) {
+    document.getElementById(el).classList.add("nonactive");
+    document.getElementById(el).classList.remove("active");
+    event.stopPropagation();
+    if (el == "left-block")
+    {
+        document.getElementsByClassName("intro-content")[0].classList.add("nonactive");
+        document.getElementsByClassName("intro-content")[0].classList.remove("active");
     }
 }
