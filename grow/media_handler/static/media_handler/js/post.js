9ba8b5373d1f736894e4b25593b2acf750645a0c
quijibo11CZE <quijibo11cze@minehub.cz>
2022-02-01 19:46:48 +0100

    Video top layout

--- grow/media_handler/static/media_handler/js/post.js
+++ grow/media_handler/static/media_handler/js/post.js
@@ -2,13 +2,17 @@
 function save()
 {
     icon = document.getElementById('post-save');
-    //num = (icon.innerHTML).replace(/(^\d+)(.+$)/i,'$1');
+    num = parseInt(icon.innerHTML);
 
     if (icon.classList.contains('saved'))
     {
         icon.classList.remove('saved');
+        var newnum = num - 1;
+        document.getElementById('post-save').innerHTML = icon.innerHTML.replace("" + num, "" + newnum);
         return;
     }
 
     icon.classList.add('saved');
+    var newnum = num + 1;
+    document.getElementById('post-save').innerHTML = icon.innerHTML.replace("" + num, "" + newnum);
 }
