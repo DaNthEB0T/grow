ca6c0492025f5eaa34e27d41184ed6bd638c6f13
Daniel Kamenov <sonnatherman04@gmail.com>
2022-02-02 01:06:54 +0100

    add ajax post saving system

--- grow/media_handler/static/media_handler/js/post.js
+++ grow/media_handler/static/media_handler/js/post.js
@@ -1,18 +1,18 @@
 // Chages color of saved icon o red :)
-function save()
-{
-    icon = document.getElementById('post-save');
-    num = parseInt(icon.innerHTML);
+// function save()
+// {
+//     icon = document.getElementById('post-save');
+//     num = parseInt(icon.innerHTML);
 
-    if (icon.classList.contains('saved'))
-    {
-        icon.classList.remove('saved');
-        var newnum = num - 1;
-        document.getElementById('post-save').innerHTML = icon.innerHTML.replace("" + num, "" + newnum);
-        return;
-    }
+//     if (icon.classList.contains('saved'))
+//     {
+//         icon.classList.remove('saved');
+//         var newnum = num - 1;
+//         document.getElementById('post-save').innerHTML = icon.innerHTML.replace("" + num, "" + newnum);
+//         return;
+//     }
 
-    icon.classList.add('saved');
-    var newnum = num + 1;
-    document.getElementById('post-save').innerHTML = icon.innerHTML.replace("" + num, "" + newnum);
-}
+//     icon.classList.add('saved');
+//     var newnum = num + 1;
+//     document.getElementById('post-save').innerHTML = icon.innerHTML.replace("" + num, "" + newnum);
+// }
--- grow/media_handler/urls.py
+++ grow/media_handler/urls.py
@@ -6,5 +6,6 @@ app_name = "media_handler"
 urlpatterns = [
     path("media", views.mh_view, name="media_test"),
     path("postu", views.post_handle_view, name="post_test"),
-    path("post/<slug:slug>", views.post_view, name="post")
+    path("post/<slug:slug>", views.post_view, name="post"),
+    path("post/<slug:slug>/toggle_save", views.post_save_view, name="post_save")
 ]
