ed3897e871a7b4fd36c1ff61ee35fd79f56d49c7
quijibo11CZE <quijibo11cze@minehub.cz>
2022-02-04 12:05:58 +0100

    Tagify tags when typed into input in postu

--- grow/media_handler/static/media_handler/js/preview.js
+++ grow/media_handler/static/media_handler/js/preview.js
@@ -31,3 +31,71 @@ if (titleInput.value)
     updateTitle();
 }
 updateImage();
+
+/**
+ * Script for making nice tags
+ */
+var tagInput = document.getElementById("tag-input");
+var actualTagInput = document.getElementById("id_tags");
+// clear acttual invisible input on page load just in case
+actualTagInput.value = "";
+
+$(tagInput).on("change keyup paste", updateTags);
+
+function updateTags()
+{
+    //set width of input to match inner text
+    var widthCalc = document.createElement('span');
+    document.getElementsByTagName("BODY")[0].appendChild(widthCalc);
+    widthCalc.classList.add('width-calc');
+    widthCalc.innerHTML = tagInput.value;
+    var width = widthCalc.getBoundingClientRect().width;
+    widthCalc.remove();
+    tagInput.style.width = "calc(1rem + " + width + "px)";
+
+    // split text into tags
+    if (tagInput.value.includes(' '))
+    {
+        // remove unwanted characters form input
+        tagInput.value = tagInput.value.replace(/[^\w\-]/gi, '');
+
+        // split the string
+        var split = tagInput.value.split(' ');
+
+        // tagify first element of array
+        if (split[0])
+        {
+            // add the tag element
+            var newtag = document.createElement('span');
+            newtag.classList.add('input-tag');
+            newtag.innerHTML = split[0].toLowerCase() + ' <i class="fas fa-times" onclick="removeTag(this.parentElement)"></i>';
+            document.getElementById('tags').appendChild(newtag);
+            
+            // delete tagified text from input
+            tagInput.value = tagInput.value.replace(split[0], "")
+        }
+
+        // delete all spaces in input
+        tagInput.value = tagInput.value.replace(/ /g, "")
+    }
+
+    // update actual input according to existing tags and tag input
+    updateTagInput()
+}
+
+function updateTagInput()
+{
+    var tagList = document.getElementsByClassName('input-tag');
+    actualTagInput.value = "";
+    for (i = 0; i < tagList.length; i++)
+    {
+        actualTagInput.value = actualTagInput.value + tagList[i].innerText + ",";
+    }
+    actualTagInput.value = actualTagInput.value + tagInput.value.toLowerCase().replace(/[^\w\-]/gi, '');
+}
+
+function removeTag(el)
+{
+    el.remove();
+    updateTagInput();
+}
