122dd990e34b84dbffe7beb65cddeaf7219cf77b
Daniel Kamenov <sonnatherman04@gmail.com>
2022-02-11 19:59:45 +0100

    bugfix

--- grow/media_handler/urls.py
+++ grow/media_handler/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "media_handler"
 
 urlpatterns = [
-    path("media", views.mh_view, name="media_test"),
+    #path("media", views.mh_view, name="media_test"),
     path("postu", views.post_handle_view, name="post_upload"),
     path("post/<slug:slug>", views.post_view, name="post"),
     path("post/<slug:slug>/toggle_save", views.post_save_view, name="post_save"),
